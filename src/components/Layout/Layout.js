0ef69856343f9231e56779a046712847ed2ce33b
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-03-29 20:08:18 +0530

    Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Wrap from '../../Hoc/Wrapper';
-import classes from './Layout.css'
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
   <Wrap>
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>
